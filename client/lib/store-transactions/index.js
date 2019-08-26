65817740ea1c7ae792ec33af9792462ea63aae71
Yoav Farhi <yoav@farhi.org>
2019-08-26 13:56:06 +0300

    send the phone number when calling createStripePaymentMethod() if we have it

--- client/lib/store-transactions/index.js
+++ client/lib/store-transactions/index.js
@@ -192,7 +192,7 @@ TransactionFlow.prototype._paymentHandlers = {
 		this._pushStep( { name: INPUT_VALIDATION, first: true } );
 		debug( 'submitting transaction with new stripe elements card' );
 
-		const { name, country, 'postal-code': zip } = newCardDetails;
+		const { name, country, 'postal-code': zip, 'phone-number': phone } = newCardDetails;
 		const paymentDetailsForStripe = {
 			name,
 			address: {
@@ -200,6 +200,10 @@ TransactionFlow.prototype._paymentHandlers = {
 				postal_code: zip,
 			},
 		};
+
+		if ( phone ) {
+			paymentDetailsForStripe.phone = phone;
+		}
 
 		try {
 			const stripePaymentMethod = await createStripePaymentMethod(
