f39161c269a60cd2391c826355b1750e363db43e
Griffith Chen <griffith.chen@automattic.com>
2025-06-20 16:47:06 +0800

    Sites List v2: Implement Views, Visitors, Likes fields (#104357)

--- client/dashboard/sites/site-fields.tsx
+++ client/dashboard/sites/site-fields.tsx
@@ -1,12 +1,55 @@
 import { useQuery } from '@tanstack/react-query';
+import { __experimentalText as Text } from '@wordpress/components';
 import { useInView } from 'react-intersection-observer';
+import { siteEngagementStatsQuery } from '../app/queries/site-stats';
 import { siteUptimeQuery } from '../app/queries/site-uptime';
 import { TextBlur } from '../components/text-blur';
+import { JetpackModules } from '../data/constants';
+import { hasJetpackModule } from '../utils/site-features';
 import type { Site } from '../data/types';
+
+function IneligibleIndicator() {
+	return <Text color="#CCCCCC">-</Text>;
+}
+
+function LoadingIndicator( { label }: { label: string } ) {
+	return <TextBlur>{ label }</TextBlur>;
+}
+
+export function EngagementStat( {
+	site,
+	type,
+}: {
+	site: Site;
+	type: 'visitors' | 'views' | 'likes';
+} ) {
+	const { ref, inView } = useInView( { triggerOnce: true, fallbackInView: true } );
+	const isEligible =
+		! site.is_deleted && ( ! site.jetpack || hasJetpackModule( site, JetpackModules.STATS ) );
+
+	const { data: stats, isLoading } = useQuery( {
+		...siteEngagementStatsQuery( site.ID ),
+		enabled: isEligible && inView,
+	} );
+
+	if ( ! isEligible ) {
+		return <IneligibleIndicator />;
+	}
+
+	const renderContent = () => {
+		if ( isLoading ) {
+			return <LoadingIndicator label="100" />;
+		}
+
+		return stats?.currentData[ type ];
+	};
+
+	return <span ref={ ref }>{ renderContent() }</span>;
+}
 
 export function Uptime( { site }: { site: Site } ) {
 	const { ref, inView } = useInView( { triggerOnce: true, fallbackInView: true } );
-	const isEligible = !! site.jetpack_modules?.includes( 'monitor' );
+	const isEligible = hasJetpackModule( site, JetpackModules.MONITOR );
 
 	const { data: uptime, isLoading } = useQuery( {
 		...siteUptimeQuery( site.ID, 'week' ),
@@ -14,15 +57,15 @@ export function Uptime( { site }: { site: Site } ) {
 	} );
 
 	if ( ! isEligible ) {
-		return '-';
+		return <IneligibleIndicator />;
 	}
 
 	const renderContent = () => {
 		if ( isLoading ) {
-			return <TextBlur>100%</TextBlur>;
+			return <LoadingIndicator label="100%" />;
 		}
 
-		return uptime ? `${ uptime }%` : '-';
+		return uptime ? `${ uptime }%` : <IneligibleIndicator />;
 	};
 
 	return <span ref={ ref }>{ renderContent() }</span>;
