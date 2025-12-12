1d46005688833af9a8e73961e1c65575d8b1b002
Xavier Lozano Carreras <8511199+xavier-lc@users.noreply.github.com>
2025-12-12 20:14:53 +0100

    Plugins Dashboard: Extract `PluginsList` components (#107672)

--- client/dashboard/plugins/manage/index.tsx
+++ client/dashboard/plugins/manage/index.tsx
@@ -6,34 +6,19 @@ import {
 } from '@automattic/api-queries';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from '@tanstack/react-router';
-import {
-	__experimentalGrid as Grid,
-	__experimentalVStack as VStack,
-	Icon,
-} from '@wordpress/components';
+import { __experimentalGrid as Grid } from '@wordpress/components';
 import { filterSortAndPaginate, View } from '@wordpress/dataviews';
-import { __, _n, sprintf } from '@wordpress/i18n';
-import { plugins as pluginIcon } from '@wordpress/icons';
-import clsx from 'clsx';
+import { __ } from '@wordpress/i18n';
 import { useMemo } from 'react';
-import { pluginRoute } from '../../app/router/plugins';
-import { Card, CardBody } from '../../components/card';
 import { OptInWelcome } from '../../components/opt-in-welcome';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
-import { SectionHeader } from '../../components/section-header';
-import SwitcherContent from '../../components/switcher/switcher-content';
-import { Text } from '../../components/text';
-import { TextBlur } from '../../components/text-blur';
-import { PluginTabs } from '../plugin';
-import { usePlugin } from '../plugin/use-plugin';
+import { PluginSites } from './components/plugin-sites';
+import { PluginSwitcher } from './components/plugin-switcher';
 import { useSitesById } from './hooks/use-sites-by-id';
 import { mapApiPluginsToDataViewPlugins } from './utils';
 import type { PluginListRow } from './types';
-import './style.scss';
 
-const ICON_SIZE = 40;
-const FALLBACK_ICON_SIZE = 30;
 const view: View = {
 	type: 'list',
 	page: 1,
@@ -71,14 +56,6 @@ export default function PluginsList() {
 		[ sitesById, sitesPlugins, fields ]
 	);
 	const selectedPluginSlug = pluginSlug || plugins[ 0 ]?.slug;
-	const {
-		icon,
-		isLoading: isLoadingPlugin,
-		plugin,
-		pluginBySiteId,
-		sitesWithThisPlugin,
-		sitesWithoutThisPlugin,
-	} = usePlugin( selectedPluginSlug );
 	const { data: marketplacePlugins } = useQuery( marketplacePluginsQuery() );
 	const { data: marketplaceSearch } = useQuery(
 		marketplaceSearchQuery( {
@@ -121,27 +98,6 @@ export default function PluginsList() {
 		} );
 	}, [ plugins, iconBySlug ] );
 
-	const decoration = useMemo( () => {
-		if ( icon ) {
-			return <img className="plugin-icon" src={ icon } alt={ plugin?.name } />;
-		} else if ( isLoadingPlugin ) {
-			return <div className="plugin-icon-placeholder" aria-hidden="true" />;
-		}
-	}, [ icon, isLoadingPlugin, plugin?.name ] );
-
-	const title = useMemo( () => {
-		if ( ! isLoadingPlugin && ! plugin ) {
-			return __( 'Plugin not found' );
-		}
-
-		return plugin ? (
-			// @ts-expect-error: Can only set one of `children` or `props.dangerouslySetInnerHTML`.
-			<Text dangerouslySetInnerHTML={ { __html: plugin.name } } />
-		) : (
-			<TextBlur>{ pluginSlug }</TextBlur>
-		);
-	}, [ isLoadingPlugin, plugin, pluginSlug ] );
-
 	return (
 		<PageLayout
 			size="large"
@@ -154,99 +110,14 @@ export default function PluginsList() {
 			notices={ <OptInWelcome tracksContext="plugins" /> }
 		>
 			<Grid columns={ 2 } gap={ 6 } templateColumns="392px 1fr">
-				<Card>
-					<CardBody className="plugin-switcher-card-body">
-						<SwitcherContent
-							itemClassName={ ( item ) =>
-								clsx( 'plugin-switcher-item', { 'is-selected': selectedPluginSlug === item.slug } )
-							}
-							searchClassName="plugin-switcher-search"
-							initialView={ view }
-							items={ pluginsWithIcon }
-							resetScroll={ false }
-							getItemUrl={ ( item ) => pluginRoute.to.replace( '$pluginId', item.slug ) }
-							renderItemMedia={ ( { item } ) => {
-								const icon = item.icon ? (
-									<img
-										src={ item.icon }
-										alt={ item.name }
-										width={ ICON_SIZE }
-										height={ ICON_SIZE }
-									/>
-								) : (
-									<Icon
-										icon={ pluginIcon }
-										size={ FALLBACK_ICON_SIZE }
-										className="plugin-icon-fallback"
-									/>
-								);
+				<PluginSwitcher
+					pluginsWithIcon={ pluginsWithIcon }
+					searchableFields={ searchableFields }
+					selectedPluginSlug={ selectedPluginSlug }
+					view={ view }
+				/>
 
-								return (
-									<div className={ clsx( 'plugin-icon-wrapper', { 'is-fallback': ! item.icon } ) }>
-										{ icon }
-									</div>
-								);
-							} }
-							renderItemTitle={ ( { item } ) => {
-								const sitesText = sprintf(
-									// translators: %(siteCount)d is the number of sites the plugin is installed on.
-									_n( '%(siteCount)d site', '%(siteCount)d sites', item.sitesCount ),
-									{ siteCount: item.sitesCount }
-								);
-
-								const updatesText = item.sitesWithPluginUpdate.length
-									? sprintf(
-											// translators: %(updateCount)d is the number of updates available.
-											_n(
-												'%(updateCount)d update available',
-												'%(updateCount)d updates available',
-												item.sitesWithPluginUpdate.length
-											),
-											{ updateCount: item.sitesWithPluginUpdate.length }
-									  )
-									: '';
-
-								return (
-									<VStack spacing={ 0 }>
-										{ /* @ts-expect-error: Can only set one of `children` or `props.dangerouslySetInnerHTML`. */ }
-										<Text
-											className="plugin-switcher-item-name"
-											dangerouslySetInnerHTML={ { __html: item.name } }
-											title={ item.name }
-										/>
-										<Text variant="muted">
-											{ updatesText ? `${ sitesText }, ${ updatesText }` : sitesText }
-										</Text>
-									</VStack>
-								);
-							} }
-							searchableFields={ searchableFields }
-							onClose={ () => {} }
-							width="auto"
-						/>
-					</CardBody>
-				</Card>
-
-				<Card>
-					<CardBody className="plugin-sites-card-body">
-						<SectionHeader
-							className="plugin-sites-card-header"
-							decoration={ decoration }
-							level={ 2 }
-							title={ title }
-						/>
-
-						<PluginTabs
-							pluginSlug={ pluginSlug }
-							isLoading={ isLoadingPlugin }
-							plugin={ plugin }
-							pluginName={ plugin?.name }
-							pluginBySiteId={ pluginBySiteId }
-							sitesWithThisPlugin={ sitesWithThisPlugin }
-							sitesWithoutThisPlugin={ sitesWithoutThisPlugin }
-						/>
-					</CardBody>
-				</Card>
+				<PluginSites selectedPluginSlug={ selectedPluginSlug } />
 			</Grid>
 		</PageLayout>
 	);
