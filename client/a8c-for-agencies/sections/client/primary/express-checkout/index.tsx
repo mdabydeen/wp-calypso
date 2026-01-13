98828939651446e62e46a61d78af7e9bae2e3e75
James Kenneth Guidaven <jkguidaven@gmail.com>
2026-01-13 21:06:10 +0800

    A4A: Implement A4A Client express checkout in WPCOM checkout. (#107734)
    * Convert client checkout logic to a reusable hook.
    
    * Add controller for A4A express checkout.
    
    * Setup CheckoutMain to handle A4A express checkout (loggedout user flow)
    
    * Fix lint error.
    
    * Add A4A branding and hide cancel button.
    
    * Trigger account creation for A4A siteless checkout.
    
    * Change URL.
    
    * Fix error with loggin user.
    
    * Add Unit testing for A4A siteless checkout.
    
    * Address PR comments.
    
    * Add unit test for disabled email address.

--- client/a8c-for-agencies/sections/client/primary/express-checkout/index.tsx
+++ client/a8c-for-agencies/sections/client/primary/express-checkout/index.tsx
@@ -2,50 +2,25 @@ import { RazorpayHookProvider } from '@automattic/calypso-razorpay';
 import { StripeHookProvider } from '@automattic/calypso-stripe';
 import { CheckoutErrorBoundary } from '@automattic/composite-checkout';
 import { useTranslate } from 'i18n-calypso';
-import A4ALogo from 'calypso/a8c-for-agencies/components/a4a-logo';
 import { getStripeConfiguration, getRazorpayConfiguration } from 'calypso/lib/store-transactions';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import CheckoutMain from 'calypso/my-sites/checkout/src/components/checkout-main';
 import { useSelector } from 'calypso/state';
-import { getCurrentUserLocale } from 'calypso/state/current-user/selectors';
+import { getCurrentUserLocale, isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import ClientCheckoutV2Error from '../../checkout-v2-error';
 import ClientCheckoutV2Placeholder from '../../checkout-v2-placeholder';
 import useClientCheckout from '../../hooks/use-client-checkout';
 
-import './style.scss';
+const EXPRESS_CHECKOUT_REDIRECT_URL = 'https://agencies.automattic.com/client/subscriptions';
 
-/**
- * Client Checkout Component using the WordPress.com checkout
- */
-function ClientCheckoutContent() {
+function ClientExpressCheckoutContent() {
 	const translate = useTranslate();
+	const userLoggedIn = useSelector( isUserLoggedIn );
 
-	const { isReady, error, emailMismatchWithReferralClient, referral } = useClientCheckout( {
-		expressMode: false,
-	} );
+	const { isReady, error } = useClientCheckout( { expressMode: ! userLoggedIn } );
 
 	if ( ! isReady ) {
 		return <ClientCheckoutV2Placeholder />;
-	}
-
-	if ( emailMismatchWithReferralClient ) {
-		return (
-			<ClientCheckoutV2Error
-				title={ translate( 'Permission denied' ) }
-				message={ translate(
-					'This referral is not intended for your account. Please make sure you sign in using {{b}}%(referralEmail)s{{/b}}.',
-					{
-						args: {
-							referralEmail: referral?.client?.email,
-						},
-						components: {
-							b: <b />,
-						},
-						comment: '%(referralEmail)s is the email of the referral client.',
-					}
-				) }
-			/>
-		);
 	}
 
 	if ( error ) {
@@ -53,24 +28,18 @@ function ClientCheckoutContent() {
 	}
 
 	return (
-		<div className="client-checkout-v2">
-			<div className="client-checkout-v2__top-bar">
-				<div className="client-checkout-v2__top-bar-logo">
-					<A4ALogo full size={ 14 } />
-				</div>
-			</div>
-			<CheckoutMain
-				sitelessCheckoutType="a4a"
-				redirectTo={ window.location.origin + '/client/subscriptions' }
-				customizedPreviousPath="/client/subscriptions"
-				siteSlug=""
-				siteId={ 0 }
-			/>
-		</div>
+		<CheckoutMain
+			sitelessCheckoutType="a4a"
+			redirectTo={ EXPRESS_CHECKOUT_REDIRECT_URL }
+			customizedPreviousPath={ EXPRESS_CHECKOUT_REDIRECT_URL }
+			isLoggedOutCart={ ! userLoggedIn }
+			siteSlug=""
+			siteId={ 0 }
+		/>
 	);
 }
 
-export default function ClientCheckoutV2() {
+export default function ClientExpressCheckout() {
 	const translate = useTranslate();
 	const locale = useSelector( getCurrentUserLocale );
 
@@ -81,7 +50,7 @@ export default function ClientCheckoutV2() {
 			<CalypsoShoppingCartProvider shouldShowPersistentErrors>
 				<StripeHookProvider fetchStripeConfiguration={ getStripeConfiguration } locale={ locale }>
 					<RazorpayHookProvider fetchRazorpayConfiguration={ getRazorpayConfiguration }>
-						<ClientCheckoutContent />
+						<ClientExpressCheckoutContent />
 					</RazorpayHookProvider>
 				</StripeHookProvider>
 			</CalypsoShoppingCartProvider>
